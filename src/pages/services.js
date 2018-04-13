c9f9d85f332c4ea3820da6fd947388d34d63f494
yaminmhd <mdyamin.drzee@gmail.com>
2018-04-13 23:59:03 +0800

    Added services page

---
+++ src/pages/services.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Services = () => (
+  <div>
+    <h1>Services</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore soluta
+      quaerat corrupti ipsam consectetur cupiditate consequuntur ea harum
+      laborum! Obcaecati magnam impedit iusto quia natus. Necessitatibus
+      consequuntur exercitationem voluptates rerum.
+    </p>
+  </div>
+)
+
+export default Services
